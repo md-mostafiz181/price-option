a5af69c22ee06c6a5f9ab8ead197e16e1da87c17
unknown <dev.mostafizurrahman@gmail.com>
2024-04-20 10:34:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl bg-orange-500 text-center">Price option </h1>
+
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </>
+  );
+}
+
+export default App;
